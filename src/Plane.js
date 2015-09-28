818d3effb8d3a0c494d172c33c9c55718dc70c8f
Dom Batten <dominic.batten@googlemail.com>
2015-09-28 17:22:57 +0100

    Test passing for plane flying and landing

---
+++ src/Plane.js
@@ -0,0 +1,34 @@
+function Plane() {
+  this.isFlying = true;
+};
+
+Plane.prototype.land = function() {
+  this.isFlying = false;
+};
+
+
+// Plane.prototype.flying = function() {
+  
+// };
+
+
+// Player.prototype.play = function(song) {
+//   this.currentlyPlayingSong = song;
+//   this.isPlaying = true;
+// };
+
+// Player.prototype.pause = function() {
+//   this.isPlaying = false;
+// };
+
+// Player.prototype.resume = function() {
+//   if (this.isPlaying) {
+//     throw new Error("song is already playing");
+//   }
+
+//   this.isPlaying = true;
+// };
+
+// Player.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
+// };

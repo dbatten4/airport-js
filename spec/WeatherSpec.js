04e7878946239433bad4903116faa1a6bf58ec6d
Proskurina <yproskurina@yahoo.com>
2015-09-29 13:41:21 +0100

    Added Weather

---
+++ spec/WeatherSpec.js
@@ -0,0 +1,14 @@
+// describe('Weather', function(){
+
+//   var weather = new Weather;
+
+//   it ('can be stormy', function(){
+//   weather = {
+//     isStormy : function(){
+//         return true
+//     }
+//  };
+//     expect(weather.isStormy()).toBe(true)
+//   });
+
+// });
